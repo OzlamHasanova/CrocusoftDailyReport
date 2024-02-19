0a3424feae372f4e1c3398f449ad1c8dc4612c43
OzlamHasanova <7qk9bme@code.edu.az>
2024-02-19 11:47:47 +0400

    update role api

--- src/main/java/az/crocusoft/CrocusoftDailyReport/controller/RoleController.java
+++ src/main/java/az/crocusoft/CrocusoftDailyReport/controller/RoleController.java
@@ -1,23 +1,23 @@
-//package az.crocusoft.CrocusoftDailyReport.controller;
-//
-//import az.crocusoft.CrocusoftDailyReport.dto.response.RoleResponse;
-//import az.crocusoft.CrocusoftDailyReport.service.RoleService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("v1/api/roles")
-//public class RoleController {
-//    private final RoleService roleService;
-//    @GetMapping
-//    public ResponseEntity<List<RoleResponse>> getAllResponse(){
-//        return ResponseEntity.ok(roleService.getAllRoles());
-//    }
-//}
+package az.crocusoft.CrocusoftDailyReport.controller;
+
+import az.crocusoft.CrocusoftDailyReport.dto.response.RoleResponse;
+import az.crocusoft.CrocusoftDailyReport.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/api/roles")
+public class RoleController {
+    private final RoleService roleService;
+    @GetMapping
+    public ResponseEntity<List<RoleResponse>> getAllResponse(){
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+}
